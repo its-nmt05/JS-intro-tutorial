9173e45db2e7908247d9f1c2eb2dc398a0c1c31e
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-05-25 18:35:03 +0530

    Added 02_dataTypes to 01_basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) # we're using node js, not browser
+
+console.log(3 
+    + 
+     3) // produce code that is readable
+
+console.log("Nirmalya")
+
+
+let name = "nirmalya"
+let age = 18
+let isLoggedIn = false
+
+
+// number => range: 2 to power 53. More than this, use bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value has not yet been assigned
+// symbol => unique
+
+
+// object 
+
+// console.log(typeof null)   # object
+// console.log(typeof undefined)   undefined
