e13e7769162ae67e1ac3d6d2c6722cd5cedd61a4
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-05-28 00:42:22 +0530

    Modified 01_functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -36,3 +36,32 @@ function loginUserMessage(userName = "sam") {
 
 // console.log(loginUserMessage("nirmalya"))
 
+function calculateCartPrice (val1, val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 700, 2000))
+
+const user = {
+    username: "nirmalya",
+    price: 199
+}
+
+function handleObject(anyObject) {
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 400, 500, 700]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+// console.log(returnSecondValue([800, 300, 1200]))
