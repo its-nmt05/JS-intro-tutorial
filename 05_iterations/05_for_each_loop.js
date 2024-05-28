6d53bba3ac64c76ccdf3339c154bcbb57f722bf8
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-05-29 01:18:25 +0530

    Added 05_for_each_loop to 05_iterations

---
+++ 05_iterations/05_for_each_loop.js
@@ -0,0 +1,37 @@
+const coding = ["python", "cpp", "javascript", "ruby", "java"]
+
+// coding.forEach(function(item) {
+//     console.log(item)
+// })
+
+// coding.forEach((item) => console.log(item))
+
+// function printMe(item) {
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)  # just give it the reference. Don't execute
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr)
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName)
+})
+
