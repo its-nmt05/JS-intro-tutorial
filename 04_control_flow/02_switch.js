cca7758f605bfc9f9f19d4fc6aec545aeb1ac398
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-05-28 20:50:30 +0530

    Added 02_switch to 04_control_flow

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("no month matched");
+        break;
+}
+
+/*
+    The purpose of the break keyword is to break the flow of execution.
+    Without the break statement, the control of the program will execute all
+    the remaining case blocks until another break statment is encountered.
+*/
+
