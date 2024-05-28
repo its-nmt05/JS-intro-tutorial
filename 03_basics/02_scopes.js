c1ae211b420c587688944f956e334207d4ea483c
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-05-28 13:45:22 +0530

    Modified 02_scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,40 @@ if (true) {         // Block scope
 // console.log("OUTER:", a) # 200
 // console.log(b)
 // console.log(c)  # 30
+
+function one() {
+    const userName = "Nirmalya"
+
+    function two() {
+        const website = "google"
+        console.log(userName)
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+if (true) {
+    const userName = "Nirmalya"
+    if (userName === "Nirmalya") {
+        const website = " google"
+        // console.log(userName + website)
+    }
+    // console.log(website)
+}
+// console.log(userName)
+
+
+// *** interesting ***
+
+// console.log(addOne(2))
+function addOne(num) {
+    return num + 1
+}
+
+// console.log(addTwo(2))   # cannot access addTwo() before declaration
+const addTwo = function(num) {
+    return num + 2
+}
+
